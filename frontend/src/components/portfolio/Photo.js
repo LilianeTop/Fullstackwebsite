efe0c21d7f421f08ece68802a5e4f7527786da82
LilianeTop <hallo@lilianetop.nl>
2021-03-11 17:20:28 +0100

    bug: resolved showing images from db, CSS issues

--- frontend/src/components/portfolio/Photo.js
+++ frontend/src/components/portfolio/Photo.js
@@ -1,48 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 function Photo(props) {
 
-    // useEffect(() => {
-    //     fetchPhotos();
-    // }, []);
-
-    // const [photo, setphoto] = useState({});
-
-    // const fetchPhoto = async () => {
-    // };
-
-    // return (
-    //     <div>
-    //         <figure>
-    //             <img className='photo' key={props.idArtpiece} src={props.selectedFile} alt={props.description}/>
-    //             <br/>
-    //         </figure>
-    //         <figcaption>
-    //             <h1>Text</h1>
-    //             {/*Text is shown so how to get the description from db?*/}
-    //             <h1>{props.description}</h1>
-    //         </figcaption>
-    //     </div>
-    // )
-// }
-
-
-// const Photo = (props) => {
-//
     return (
 
         //FIXME: how to show description? and how to display underneath photo?
         //FIXME how to show photo
-        <div>
-            <figure>
-                <img className='photo' key={props.idArtpiece} src={props.selectedFile} alt={props.description}/>
-                <br/>
-            </figure>
+        //return  (
+        //     <figure>
+        //     <img key={props.id} src={props.picture} alt={props.alt}/>
+        //     <figcaption>{props.alt}</figcaption>
+        //     </figure>
+        //   );
+
+        <figure>
+            <img key={props.id} src={props.selectedFile} alt={props.description}/>
             <figcaption>
                 {/*Text is shown so how to get the description from db?*/}
-                <h3>Beschrijving van foto is: {props.description}</h3>
+                Beschrijving van foto is: {props.alt}
             </figcaption>
-        </div>
+        </figure>
+
     )
 }
+
 export default Photo;
