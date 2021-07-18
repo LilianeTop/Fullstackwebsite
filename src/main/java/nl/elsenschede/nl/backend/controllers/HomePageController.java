0babf94943746209106039ae1526362f165cee75
LilianeTop <hallo@lilianetop.nl>
2021-07-18 16:17:14 +0200

    changed from restcontroller to controller

--- src/main/java/nl/elsenschede/nl/backend/controllers/HomePageController.java
+++ src/main/java/nl/elsenschede/nl/backend/controllers/HomePageController.java
@@ -1,12 +1,13 @@
 package nl.elsenschede.nl.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping({"/api"})
 @CrossOrigin(origins="https://els-enschede.herokuapp.com/")
 public class HomePageController {
