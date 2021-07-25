1e875afbf7ebfb4efe97615d6e96858984a3d815
LilianeTop <hallo@lilianetop.nl>
2021-07-25 12:20:59 +0200

    controller back to String slash

--- src/main/java/nl/elsenschede/nl/backend/controllers/HomePageController.java
+++ src/main/java/nl/elsenschede/nl/backend/controllers/HomePageController.java
@@ -3,16 +3,7 @@ package nl.elsenschede.nl.backend.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletResponse;
-
-//@RequestMapping("/")
-//public ModelAndView welcome() {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("bakara");
-//        return modelAndView;
-//        }
 @RestController
 @RequestMapping({"/api"})
 @CrossOrigin(origins="https://els-enschede.herokuapp.com/")
@@ -23,11 +14,10 @@ public class HomePageController {
         super();
     }
 
-    @RequestMapping(value = "/")
-    public ModelAndView homepageHandle() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/");
-        return modelAndView;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homepageHandle() {
+
+        return "redirect:/";
     }
 }
 
